8712db3f7c50bbdc8547cc33da437872819bc323
Andre-Valochkin <avalockin@gmail.com>
2024-10-26 18:05:03 +0300

    создал первый PROPS

--- src/components/MainBlock/Profile/ProfilePosts/ProfilePosts.js
+++ src/components/MainBlock/Profile/ProfilePosts/ProfilePosts.js
@@ -4,11 +4,11 @@ import ProfilePostsElement from './ProfilePostsElement/ProfilePostsElement.js';
 const ProfilePosts = () => {
 	return (
 		<div className='profilePosts__wrapper'>
-			<ProfilePostsElement />
-			<ProfilePostsElement />
-			<ProfilePostsElement />
-			<ProfilePostsElement />
-			<ProfilePostsElement />
+			<ProfilePostsElement massege='Первый пропс переданый мной!' like='23' name='Bася' age='34' />
+			<ProfilePostsElement massege='Второй пропс переданый мной!' like='24' name='Олег' age='43' />
+			<ProfilePostsElement massege='Третий и ничем не отличающийся от двух предыдущих, пропс переданый мной!' like='37' name='Снижана' age='' />
+			<ProfilePostsElement massege='Четвертый пропс переданый мной!' like='22' name='Виталик' age='45' />
+			<ProfilePostsElement massege='Пятый и пожалуй последний. Но тоже такой же самый как и предыдущие четыри пропса. Вообщем на создавал одинаковых пропсов и радуюсь теперь. Какой я молодец...!!!' like='146' name='Андрей' age='36' />
 		</div>
 	);
 }
--- src/components/MainBlock/Profile/ProfilePosts/ProfilePostsElement/ProfilePostsElement.js
+++ src/components/MainBlock/Profile/ProfilePosts/ProfilePostsElement/ProfilePostsElement.js
@@ -1,13 +1,20 @@
 import './ProfilePostsElement.css';
 
-const ProfilePostsElement = () => {
+const ProfilePostsElement = (props) => {
 	return (
 		<div className='profilePostsElement__wrapper'>
 			<div className='profilePostsElement__imagesBlock'>
 				<img className='profilePostsElement__img' src='images/userAva/FofoUser.png' alt='Foto User' />
 			</div>
-			<div className='profilePostsElement__messagesUser'>
-				Будущее сообщение пользователя
+			<div className='profilePostsElement__infoBlokUser'>
+				<div className='profilePostsElement__nameAgeBlock'>
+					<h2 className='profilePostsElement__nameUser'>{props.name}</h2>
+					<span className='profilePostsElement__ageUser'>возраст: <span className='profilePostsElement__ageUserNumber'>{props.age}</span></span>
+				</div>
+				<div className='profilePostsElement__messagesUser'>
+					{props.massege}
+				</div>
+				<span className='profilePostsElement__blockLike'>лайки: <span>{props.like}</span></span>
 			</div>
 		</div>
 	);
